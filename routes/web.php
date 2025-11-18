f1b6c17ce70369faa70a00468ee55133dc84b3c2
Antonio Góngora <antonio.gongora1491@gmail.com>
2025-11-18 13:07:23 -0600

    redirección de home a formulario de registro

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('participants.create');
 });
 
 Route::get('/dashboard', function () {
